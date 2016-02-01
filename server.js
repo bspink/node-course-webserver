0da0d623beadc63c33898172e3d03fa8d3cfe715
Bradley Spink <bradley.spink@gmail.com>
2016-02-01 19:06:06 +1000

    removing useless comments and updating stupid working to stupider wording

--- server.js
+++ server.js
@@ -8,13 +8,11 @@ var middleware = {
         next();
     },
     logger: function (req, res, next) {
-        // 
         console.log('Request: ' + new Date().toString() + ' '+ req.method + ' ' + req.originalUrl);
         next();
     }
 };
 
-//app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
